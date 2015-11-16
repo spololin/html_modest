a647c0f87e01f12727fddee08555da8d93cefcf3
LeXX <spololin@outlook.com>
2015-11-17 00:01:57 +0300

    all verstka

--- _js/script.js
+++ _js/script.js
@@ -1,3 +1,16 @@
-/**
- * Created by LeXX on 15.11.2015.
- */
+function initialize() {
+    var mapOptions = {
+        zoom: 12,
+        center: new google.maps.LatLng(23.02255, 72.57087)
+    }
+    var map = new google.maps.Map(document.getElementById('map'),
+        mapOptions);
+    //var image = "http://i.neoseeker.com/mgv/499846/846/33/15847_179440563793_179437393793_2772323_4548598_n_icon.jpg";
+    var myLatLng = new google.maps.LatLng(23.023, 72.571);
+    var beachMarker = new google.maps.Marker({
+        position: myLatLng,
+        map: map
+        //icon: image
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
